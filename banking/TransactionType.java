6491d29e41f60ecb05c67cff8c870cdc75b531a1
Helene <hhoffman2@hawkmail.hccfl.edu>
2019-04-27 14:31:23 -0400

    Tests for Transaction Type class, all types in enum.

--- banking/TransactionType.java
+++ banking/TransactionType.java
@@ -1,9 +1,42 @@
 package banking;
-
+import static org.junit.Assert.*;
+import org.junit.*;
 /** A list of the possible types of transactions in the banking simulation.
  * @author wpollock
+ * @edited by Helene Hoffman
  *
  */
 public enum TransactionType {
     DEPOSIT, WITHDRAWAL, INTEREST, CHECK, FEE, PENALTY, ADJUSTMENT;
+    
+   //tests Transaction type Deposit.
+   @Test
+   public void testEnumTypeDeposit() {
+    assertEquals("DEPOSIT", banking.TransactionType.DEPOSIT);
+}
+   //tests Transaction type Withdrawal.
+   @Test
+   public void testEnumTypeWithdrawal() {
+    assertEquals("WITHDRAWAL", banking.TransactionType.WITHDRAWAL);
+}  
+   //tests Transaction type Check.
+   @Test
+   public void testEnumTypeCheck() {
+    assertEquals("CHECK", banking.TransactionType.CHECK);
+}  
+   //tests Transaction type Fee.
+   @Test
+   public void testEnumTypeFee() {
+    assertEquals("FEE", banking.TransactionType.FEE);
+} 
+   //tests Transaction type Penalty.
+   @Test
+   public void testEnumTypePenalty() {
+    assertEquals("PENALTY", banking.TransactionType.PENALTY);
+} 
+   //tests Transaction type Adjustment.
+   @Test
+   public void testEnumTypeAdjustment() {
+    assertEquals("PENALTY", banking.TransactionType.ADJUSTMENT);
+} 
 }
