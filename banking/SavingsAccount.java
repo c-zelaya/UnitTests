cb18e05d404fbab40a9e015517e3e09fb9070632
Helene <hhoffman2@hawkmail.hccfl.edu>
2019-04-27 16:30:30 -0400

    Tests for Savings Account class.

--- banking/SavingsAccount.java
+++ banking/SavingsAccount.java
@@ -1,9 +1,12 @@
 package banking;
 
 import static banking.TransactionType.*;
+import static org.junit.Assert.*;
+import org.junit.Test;
 
 /**
  * @author wpollock
+ * @edited by Helene Hoffman
  *
  */
 public class SavingsAccount extends Account {
@@ -31,7 +34,46 @@ public class SavingsAccount extends Account {
         super(cust, initialBalance, description);
         // Stub
     }
-
+        //tests creation of Savings Account
+        @Test
+        public void testSavingsAccount() {
+            SavingsAccount expectedSavingsAccount = new SavingsAccount("Jane Doe", 100, "Savings Account", DEFAULT_INTEREST_RATE);
+            assertEquals(expectedSavingsAccount, SavingsAccount());
+            }
+        
+        //tests customer specified for account
+        @Test
+        public void testCustomer() {
+            SavingsAccount testSavingsAccount = new SavingsAccount("Jane Doe", 100, "Savings Account", DEFAULT_INTEREST_RATE);
+            String expectedName = "Jane Doe";
+            assertEquals(expectedName, SavingsAccount.cust);
+            
+            }
+        
+        //tests initial balance of account
+        @Test
+        public void testInitialBalance() {
+            SavingsAccount testSavingsAccount = new SavingsAccount("Jane Doe", 100, "Savings Account", DEFAULT_INTEREST_RATE);
+            double expectedBalance = 100;
+            assertEquals(expectedBalance, SavingsAccount.initialBalance);
+            }
+        
+        //tests description specified for account
+        @Test
+        public void testAccountDescription() {
+            SavingsAccount testSavingsAccount = new SavingsAccount("Jane Doe", 100, "Savings Account", DEFAULT_INTEREST_RATE);
+            String expectedDescription = "Savings Account";
+            assertEquals(expectedDescription, SavingsAccount.description);
+            }
+        
+        //tests interest rate for account
+        @Test
+        public void testInterestRate() {
+            SavingsAccount testSavingsAccount = new SavingsAccount("Jane Doe", 100, "Savings Account", DEFAULT_INTEREST_RATE);
+            double expectedInterestRate = DEFAULT_INTEREST_RATE;
+            assertEquals(expectedInterestRate, SavingsAccount.getDefaultInterestRate());
+            }
+        
     @Override
     public void deposit (double amount) {
         // Stub
@@ -46,10 +88,19 @@ public class SavingsAccount extends Account {
      * monthly interest rate.
      * @param rate Interest rate to apply, as a percentage (e.g. 2.1".
      */
+   
     public void addInterestTransaction (double rate) {
         // Stub
-    }
 
+    }
+        //tests addition of interest transaction for account
+        @Test
+        public void testInterestTransaction() {
+        SavingsAccount testSavingsAccount = new SavingsAccount("Jane Doe", 100, "Savings Account", DEFAULT_INTEREST_RATE);
+        Transaction INTEREST_PAYMENT = new Transaction();
+        double rate = DEFAULT_INTEREST_RATE;
+        assertTrue ("Add interest transaction?", rate == DEFAULT_INTEREST_RATE);
+    }
     /**
      * @return the interestRate
      */
